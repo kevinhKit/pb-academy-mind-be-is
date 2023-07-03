00c2414e674e3c0592bd924f637c1da7850dbf99
Kevin Hernandez <kevin.davidhr@gmail.com>
2023-07-03 01:14:36 -0600

    dockerization and connection to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+      // -----------------------
+      "extra": {
+        "precision": {
+          "integer": 32,
+          "decimal": 10
+        }
+      }
+    })
+  ],
+
   controllers: [AppController],
+
   providers: [AppService],
+
 })
 export class AppModule {}
