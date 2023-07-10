3c408e2e009f3f901d56e7ad5816c6edd585173d
erick molina <eralejo2003@gmail.com>
2023-07-10 13:38:26 -0600

    PATCH optional attributes

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,12 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsString } from 'class-validator';
+import { IsString, IsOptional } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
   @IsString({ message: 'La contraseña debe de ser de tipo texto.' })
   password?: string;
+  @IsOptional()
   @IsString({ message: 'La nueva contraseña debe de ser de tipo texto.' })
   newPassword?: string;
+  @IsOptional()
   @IsString({ message: 'La descripción debe de ser de tipo texto.' })
   description?: string;
 }
