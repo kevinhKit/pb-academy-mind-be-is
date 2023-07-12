cd00d62be04bec774e8e79c02ff7a36c7939f4e9
Kevin Hernandez <kevin.davidhr@gmail.com>
2023-07-11 21:31:42 -0600

    validation of empty field error messages in the user creation DTO

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,33 +1,41 @@
-import { IsOptional, IsString } from "class-validator";
-
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateUserDto {
 
     
+    @IsNotEmpty({message: "No envió o dejo vacio el campo DNI"})
     @IsString({message: "El DNI debe ser una cadena de texto."})
     dni: string;
     
+    @IsNotEmpty({message: "No envió o dejo vacio el campo primer nombre"})
     @IsString({message: "El primer nombre debe ser una cadena de texto."})
     firstName: string;
     
+    @IsNotEmpty({message: "No envió o dejo vacio el campo segundo nombre"})
     @IsString({message: "El segundo nombre debe ser una cadena de texto."})
     secondName: string;
     
+    @IsNotEmpty({message: "No envió o dejo vacio el campo primer apellido"})
     @IsString({message: "El primer apellido debe ser una cadena de texto."})
     firstLastName: string;
     
+    @IsNotEmpty({message: "No envió o dejo vacio el campo segundo apellido"})
     @IsString({message: "El segundo apellido debe ser una cadena de texto."})
     secondLastName: string;
     
+    @IsNotEmpty({message: "No envió o dejo vacio el campo correo electrónico"})
     @IsString({message: "El correo electónico debe ser una cadena de texto."})
     email: string;
     
+    @IsNotEmpty({message: "No envió o dejo vacio el campo dirección"})
     @IsString({message: "La dirección debe ser una cadena de texto."})
     address: string;
     
+    @IsNotEmpty({message: "No envió o dejo vacio el campo Teléfono"})
     @IsString({message: "El Telefono debe ser una cadena de texto."})
     phone: string;
     
+    @IsNotEmpty({message: "No envió o dejo vacio el campo descripción"})
     @IsString({message: "La descripción debe ser una cadena de texto."})
     description: string;
     
