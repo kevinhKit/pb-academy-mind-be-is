6d028ede1caaa6c887cc9c0868cd63443c9db327
Kevin Hernandez <kevin.davidhr@gmail.com>
2023-07-11 21:28:59 -0600

    feat: assigning attributes to the user creation DTO (admins)

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,52 @@
-export class CreateUserDto {}
+import { IsOptional, IsString } from "class-validator";
+
+
+export class CreateUserDto {
+
+    
+    @IsString({message: "El DNI debe ser una cadena de texto."})
+    dni: string;
+    
+    @IsString({message: "El primer nombre debe ser una cadena de texto."})
+    firstName: string;
+    
+    @IsString({message: "El segundo nombre debe ser una cadena de texto."})
+    secondName: string;
+    
+    @IsString({message: "El primer apellido debe ser una cadena de texto."})
+    firstLastName: string;
+    
+    @IsString({message: "El segundo apellido debe ser una cadena de texto."})
+    secondLastName: string;
+    
+    @IsString({message: "El correo electónico debe ser una cadena de texto."})
+    email: string;
+    
+    @IsString({message: "La dirección debe ser una cadena de texto."})
+    address: string;
+    
+    @IsString({message: "El Telefono debe ser una cadena de texto."})
+    phone: string;
+    
+    @IsString({message: "La descripción debe ser una cadena de texto."})
+    description: string;
+    
+
+
+
+
+    @IsOptional()
+    @IsString({message: "El photoOne debe ser una cadena de texto."})
+    photoOne: string;
+    
+    @IsOptional()
+    @IsString({message: "El photoTwo debe ser una cadena de texto."})
+    photoTwo: string;
+    
+    @IsOptional()
+    @IsString({message: "El photoThree debe ser una cadena de texto."})
+    photoThree: string;
+
+
+
+}
