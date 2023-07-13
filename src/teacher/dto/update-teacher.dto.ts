e5405696c0e9c1ee86333ca45be022edb642746f
erick molina <eralejo2003@gmail.com>
2023-07-12 22:06:55 -0600

    arreglando creacion emails & elminando admin de teachers

--- src/teacher/dto/update-teacher.dto.ts
+++ src/teacher/dto/update-teacher.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType, PickType } from '@nestjs/mapped-types';
 import { CreateTeacherDto } from './create-teacher.dto';
 // admin: 'isCoordinator','isBoss','isAdmin'
-export class UpdateTeacherDto extends PartialType(PickType(CreateTeacherDto,['email','address','phone','video'])) {}
+export class UpdateTeacherDto extends PartialType(
+  PickType(CreateTeacherDto, ['email', 'address', 'phone', 'video']),
+) {}
