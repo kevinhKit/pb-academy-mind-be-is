8181a4c8aafede893e23b37d6b3be0ac6015f7db
Kevin Hernandez <kevin.davidhr@gmail.com>
2023-07-15 10:12:48 -0600

    refactor: mailing credentials

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,16 +1,19 @@
+import { Logger } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
+require('dotenv').config();
+
+const logger = new Logger('loggerTransporter');
 
 export const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT),
+  secure: Boolean(process.env.SMTP_SECURE),
   auth: {
-    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-    user: 'eralejo2003@gmail.com',
-    pass: 'pgiljdvsyeucqgvk',
+    user: process.env.SMTP_USERNAME,
+    pass: process.env.SMTP_PASSWORD,
   },
 });
 
 transporter.verify().then(() => {
-  console.log('ready for send emails');
+  logger.log('Ready for send emails')
 });
