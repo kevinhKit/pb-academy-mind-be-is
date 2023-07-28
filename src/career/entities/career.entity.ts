da9df20f24a05d6dd9f978e0c3b36c92d29b3b9a
Kevin Hernandez <kevin.davidhr@gmail.com>
2023-07-28 16:23:46 -0600

    fix: Entity CareerChange and CenterChange

--- src/career/entities/career.entity.ts
+++ src/career/entities/career.entity.ts
@@ -1,6 +1,4 @@
 import { CareerClass } from "src/career-class/entities/career-class.entity";
-import { StudentCareer } from "src/student-career/entities/student-career.entity";
-import { TeachingCareer } from "src/teaching-career/entities/teaching-career.entity";
 import { Column, Entity, ManyToOne, OneToMany, PrimaryColumn } from "typeorm";
 
 @Entity('Career')
