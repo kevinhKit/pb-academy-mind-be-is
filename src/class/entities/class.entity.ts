aca6a1175d5dde051f5cb1c87700d9895d69c2da
erick molina <eralejo2003@gmail.com>
2023-07-31 15:18:21 -0600

    AMS-141 crear clases

--- src/class/entities/class.entity.ts
+++ src/class/entities/class.entity.ts
@@ -1,46 +1,48 @@
-import { CareerClass } from "src/career-class/entities/career-class.entity";
-import { RequirementClass } from "src/requirement-class/entities/requirement-class.entity";
-import { Section } from "src/section/entities/section.entity";
-import { Column, Entity, OneToMany, PrimaryColumn } from "typeorm";
+import { CareerClass } from 'src/career-class/entities/career-class.entity';
+import { RequirementClass } from 'src/requirement-class/entities/requirement-class.entity';
+import { Section } from 'src/section/entities/section.entity';
+import {
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('Class')
 export class Class {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
 
-    @PrimaryColumn('text')
-    id: string;
+  @Column('text', {
+    nullable: false,
+    unique: true,
+  })
+  code: string;
 
-    @Column('text',{
-        nullable: false,
-        unique: true
-    })
-    code: string;
-    
-    
-    @Column('text')
-    name: string;
-    
-    
-    @Column('smallint',{
-        nullable: false
-    })
-    valueUnits: string;
+  @Column('text')
+  name: string;
 
+  @Column('smallint', {
+    nullable: false,
+  })
+  valueUnits: string;
 
-    @OneToMany(
-        () => RequirementClass, (requirementClass) => requirementClass.idRequirement
-    )
-    requirementClass: RequirementClass;
+  @OneToMany(
+    () => RequirementClass,
+    (requirementClass) => requirementClass.idRequirement,
+  )
+  requirementClass: RequirementClass;
 
-    @OneToMany(
-        () => RequirementClass, (requirementClass) => requirementClass.idClass
-    )
-    classCurrent: RequirementClass;
+  @OneToMany(
+    () => RequirementClass,
+    (requirementClass) => requirementClass.idClass,
+  )
+  classCurrent: RequirementClass;
 
-    @OneToMany(() => CareerClass, (careerClass) => careerClass.idClass)
-    careerClass: CareerClass;
+  @OneToMany(() => CareerClass, (careerClass) => careerClass.idClass)
+  careerClass: CareerClass;
 
-    @OneToMany(() => Section, (section) => section.idClass)
-    section: Section;
-
-
+  @OneToMany(() => Section, (section) => section.idClass)
+  section: Section;
 }
