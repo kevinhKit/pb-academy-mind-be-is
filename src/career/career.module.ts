c8b32a44ebfe1bc28b863ae88bca9f4135261f77
erick molina <eralejo2003@gmail.com>
2023-08-01 22:44:20 -0600

    AMS-159 creando carreras aceptando sus centros regionales

--- src/career/career.module.ts
+++ src/career/career.module.ts
@@ -4,10 +4,19 @@ import { CareerService } from './career.service';
 import { CareerController } from './career.controller';
 import { Career } from './entities/career.entity';
 import { TeachingCareer } from 'src/teaching-career/entities/teaching-career.entity';
+import { RegionalCenter } from 'src/regional-center/entities/regional-center.entity';
+import { CenterCareer } from 'src/center-career/entities/center-career.entity';
 
 @Module({
   controllers: [CareerController],
   providers: [CareerService],
-  imports: [TypeOrmModule.forFeature([Career, TeachingCareer])]
+  imports: [
+    TypeOrmModule.forFeature([
+      Career,
+      TeachingCareer,
+      RegionalCenter,
+      CenterCareer,
+    ]),
+  ],
 })
 export class CareerModule {}
--- src/career/dto/create-career.dto.ts
+++ src/career/dto/create-career.dto.ts
@@ -1,26 +1,38 @@
-import { IsNotEmpty, IsNumberString, IsOptional, IsString } from "class-validator";
+import {
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { RegionalCenter } from 'src/regional-center/entities/regional-center.entity';
 
 export class CreateCareerDto {
+  @IsString({
+    each: true,
+    message: 'Los centros regionales deben de ser un arreglo de strings',
+  })
+  @IsNotEmpty({ message: 'El id del centro regional es un campo obligatorio.' })
+  centerId: RegionalCenter[];
 
+  @IsString({ message: 'El id de la Carrera debe ser de tipo texto.' })
+  @IsNotEmpty({ message: 'No envió o dejo vacio el id de la Carrera' })
+  id: string;
 
-    @IsString({ message: 'El id de la Carrera debe ser de tipo texto.' })
-    @IsNotEmpty({message: "No envió o dejo vacio el id de la Carrera"})
-    id: string;
+  @IsString({ message: 'El nombre de la Carrera debe ser de tipo texto.' })
+  @IsNotEmpty({ message: 'No envió o dejo vacio el nombre de la Carrera' })
+  name: string;
 
+  @IsString({ message: 'El código de la carrera debe ser de tipo texto.' })
+  @IsNotEmpty({ message: 'No envió o dejo vacio el Código de la Carrera' })
+  raceCode: string;
 
-    @IsString({ message: 'El nombre de la Carrera debe ser de tipo texto.' })
-    @IsNotEmpty({message: "No envió o dejo vacio el nombre de la Carrera"})
-    name: string;
-
-
-    @IsString({ message: 'El código de la carrera debe ser de tipo texto.' })
-    @IsNotEmpty({message: "No envió o dejo vacio el Código de la Carrera"})
-    raceCode: string;
-
-    @IsOptional()
-    @IsNumberString({},{ message: 'LA nota minima de ingreso debe ser de tipo Numero.' })
-    @IsNotEmpty({message: "No envió o dejo vacio el campo no ta minima de ingreso"})
-    minimumIncomeValue: string;
-
-
+  @IsOptional()
+  @IsNumberString(
+    {},
+    { message: 'LA nota minima de ingreso debe ser de tipo Numero.' },
+  )
+  @IsNotEmpty({
+    message: 'No envió o dejo vacio el campo no ta minima de ingreso',
+  })
+  minimumIncomeValue: string;
 }
