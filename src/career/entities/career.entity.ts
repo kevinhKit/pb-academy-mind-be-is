0c19d35fabdbaf74a22013b9d4d360f4ac77848f
erick molina <eralejo2003@gmail.com>
2023-08-01 21:45:28 -0600

    AMS-158 crear clases con carrera

--- src/career/entities/career.entity.ts
+++ src/career/entities/career.entity.ts
@@ -1,41 +1,38 @@
-import { CareerClass } from "src/career-class/entities/career-class.entity";
-import { Column, Entity, ManyToOne, OneToMany, PrimaryColumn } from "typeorm";
+import { CareerClass } from 'src/career-class/entities/career-class.entity';
+import { Column, Entity, ManyToOne, OneToMany, PrimaryColumn } from 'typeorm';
 
 @Entity('Career')
 export class Career {
+  @PrimaryColumn('text', {
+    unique: true,
+    nullable: false,
+  })
+  id: string;
 
-    @PrimaryColumn('text',{
-        unique: true,
-        nullable: false
-    })
-    id: string;
+  @Column('text', {
+    unique: true,
+    nullable: false,
+  })
+  raceCode: string;
 
-    @Column('text',{
-        unique: true,
-        nullable: false
-    })
-    raceCode: string;
+  @Column('text', {
+    nullable: false,
+    unique: true,
+  })
+  name: string;
 
-    @Column('text',{
-        nullable: false,
-        unique: true
-    }
-    )
-    name: string;
+  @Column({
+    type: 'smallint',
+    default: 700,
+    nullable: false,
+  })
+  minimumIncomeValue: number;
 
-    @Column({
-        type: 'smallint',
-        default: 700,
-        nullable: false
-    })
-    minimumIncomeValue: number;
+  @OneToMany(() => CareerClass, (careerClass) => careerClass.idClass)
+  careerClass: CareerClass[];
 
-    @OneToMany(() => CareerClass, (careerClass) => careerClass.idClass)
-    careerClass: CareerClass[];
-
-    @Column('timestamptz',{
-        default: () => "current_timestamp"
-      })
-    create_at: Date;
-
+  @Column('timestamptz', {
+    default: () => 'current_timestamp',
+  })
+  create_at: Date;
 }
--- src/class/class.module.ts
+++ src/class/class.module.ts
@@ -5,10 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Class } from './entities/class.entity';
 import { SharedModule } from 'src/shared/shared.module';
 import { RequirementClass } from 'src/requirement-class/entities/requirement-class.entity';
+import { Career } from 'src/career/entities/career.entity';
+import { CareerClass } from 'src/career-class/entities/career-class.entity';
 
 @Module({
   controllers: [ClassController],
   providers: [ClassService],
-  imports: [TypeOrmModule.forFeature([Class, RequirementClass]), SharedModule],
+  imports: [
+    TypeOrmModule.forFeature([Class, RequirementClass, Career, CareerClass]),
+    SharedModule,
+  ],
 })
 export class ClassModule {}
