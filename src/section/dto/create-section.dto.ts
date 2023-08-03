44d99344022e9f4136584f6ccaeaf0f63b740738
erick molina <eralejo2003@gmail.com>
2023-08-02 18:06:29 -0600

    Crear secciones, devolver todas las secciones y devolver una seccion

--- src/section/dto/create-section.dto.ts
+++ src/section/dto/create-section.dto.ts
@@ -1,47 +1,39 @@
-import { IsArray, IsEnum, IsIn, IsNotEmpty, IsString } from 'class-validator';
-
-enum daysClass {
-  LU = 'LU',
-  MA = 'MA',
-  MI = 'MI',
-  JU = 'JU',
-  SA = 'SA',
-  DO = 'DO',
-}
+import { IsArray, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Class } from 'src/class/entities/class.entity';
+import { Classroom } from 'src/classroom/entities/classroom.entity';
+import { Period } from 'src/period/entities/period.entity';
+import { Teacher } from 'src/teacher/entities/teacher.entity';
 
 export class CreateSectionDto {
-  @IsString({ message: 'El id del periodo debe ser de tipo texto.' })
+  @IsNumber({}, { message: 'El id del periodo debe ser de tipo numerico.' })
   @IsNotEmpty({ message: 'No envió o dejo vacio el id del periodo' })
-  idPeriod: string;
+  idPeriod: Period;
 
-  @IsString({ message: 'El código de la sección debe ser de tipo texto.' })
-  @IsNotEmpty({ message: 'No envió o dejo vacio el código de la sección' })
-  codeSection: string;
-
-  @IsString({ message: 'El id de la clase debe ser de tipo texto.' })
+  @IsNumber({}, { message: 'El id de la clase debe ser de tipo numerico.' })
   @IsNotEmpty({ message: 'No envió o dejo vacio el id de la clase' })
-  idClass: string;
+  idClass: Class;
 
   @IsString({ message: 'El id del docente debe ser de tipo texto.' })
   @IsNotEmpty({ message: 'No envió o dejo vacio el id del docente' })
-  idTeacher: string;
+  idTeacher: Teacher;
 
   @IsString({ message: 'El Numero de cupos debe ser de tipo texto.' })
   @IsNotEmpty({ message: 'No envió o dejo vacio el campo número de cupos' })
   space: string;
 
-  @IsArray()
-  @IsIn(['Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa', 'Do'], {
-    each: true,
-    message: 'No se ha enviado dias de la semana validos',
-  })
-  days: daysClass[];
+  @IsString({ message: 'Los dias de la clase deben ser de tipo texto' })
+  @IsNotEmpty({ message: 'Los dias de la seccion no pueden ir vacios' })
+  days: string;
 
   @IsString({ message: 'El Aula debe ser de tipo texto.' })
   @IsNotEmpty({ message: 'No envió o dejo vacio el campo aula' })
-  idClassroom: string;
+  idClassroom: Classroom;
 
   @IsString({ message: 'La hora debe ser de tipo texto.' })
   @IsNotEmpty({ message: 'No envió o dejo vacio el campo hora' })
   hour: string;
+
+  @IsString({ message: 'La hora debe ser de tipo texto.' })
+  @IsNotEmpty({ message: 'No envió o dejo vacio el campo hora' })
+  finalHour: string;
 }
--- src/section/section.controller.ts
+++ src/section/section.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { SectionService } from './section.service';
 import { CreateSectionDto } from './dto/create-section.dto';
 import { UpdateSectionDto } from './dto/update-section.dto';
@@ -19,7 +27,7 @@ export class SectionController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.sectionService.findOne(+id);
+    return this.sectionService.findOne(id);
   }
 
   @Patch(':id')
