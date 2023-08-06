c8d5876391aff99de86ea556bacad6079c2f8ab8
Kevin Hernandez <kevin.davidhr@gmail.com>
2023-08-06 13:15:38 -0600

    feat: AMS-122 create careerChange

--- src/career-change/dto/create-career-change.dto.ts
+++ src/career-change/dto/create-career-change.dto.ts
@@ -1,1 +1,21 @@
-export class CreateCareerChangeDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateCareerChangeDto {
+
+    @IsString({ message: 'El número de cuenta debe ser de tipo texto.' })
+    @IsNotEmpty({message: "No envió o dejo vacio el campo número de cuenta"})
+    accountNumber: string;
+
+    @IsString({ message: 'El id de la carrera debe ser de tipo texto.' })
+    @IsNotEmpty({message: "No envió o dejo vacio el campo id de carrera"})
+    idCareer: string;
+
+    @IsString({ message: 'La justificación debe ser de tipo texto.' })
+    @IsNotEmpty({message: "No envió o dejo vacio el campo justificación"})
+    justification: string;
+
+    @IsString({ message: 'el pdf de justificación debe ser una cadena de texto debe ser de tipo texto.' })
+    @IsNotEmpty({message: "No envió o dejo vacio el campo justificación de pdf"})
+    justificationPdf: string;
+
+}
