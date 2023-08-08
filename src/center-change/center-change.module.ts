5102fd261774dc10bfdb942335f76afc1a08989e
Kevin Hernandez <kevin.davidhr@gmail.com>
2023-08-08 16:09:40 -0600

    feat: verify if when requesting a change of regional center the student already has a pending career change

--- src/center-change/center-change.module.ts
+++ src/center-change/center-change.module.ts
@@ -6,10 +6,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentCareer } from 'src/student-career/entities/student-career.entity';
 import { CenterCareer } from 'src/center-career/entities/center-career.entity';
 import { Student } from 'src/student/entities/student.entity';
+import { CareerChange } from 'src/career-change/entities/career-change.entity';
 
 @Module({
   controllers: [CenterChangeController],
   providers: [CenterChangeService],
-  imports: [TypeOrmModule.forFeature([CenterChange,Student,CenterCareer,StudentCareer])],
+  imports: [TypeOrmModule.forFeature([CenterChange,CareerChange,Student,CenterCareer,StudentCareer])],
 })
 export class CenterChangeModule {}
