f1cfb48eacdfbab80f4182bc59bf928c725755af
Kevin Hernandez <kevin.davidhr@gmail.com>
2023-08-08 16:15:43 -0600

    feat: verify that when making a career change, the student is not going through a career change process

--- src/career-change/career-change.module.ts
+++ src/career-change/career-change.module.ts
@@ -6,10 +6,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Student } from 'src/student/entities/student.entity';
 import { CenterCareer } from 'src/center-career/entities/center-career.entity';
 import { StudentCareer } from 'src/student-career/entities/student-career.entity';
+import { CenterChange } from 'src/center-change/entities/center-change.entity';
 
 @Module({
   controllers: [CareerChangeController],
   providers: [CareerChangeService],
-  imports: [TypeOrmModule.forFeature([CareerChange,Student,CenterCareer,StudentCareer])],
+  imports: [TypeOrmModule.forFeature([CareerChange,CenterChange,Student,CenterCareer,StudentCareer])],
 })
 export class CareerChangeModule {}
